0c3f21f24e9d993cebf05be8c4c8019e6f705cf2
Antonio Attianese <antatt@edu.novia.fi>
2023-01-26 15:31:03 +0200

    hometest main should be unchanged

---
+++ ToDoApplications/ToDoApi/Models/TodoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ToDoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+
+}
