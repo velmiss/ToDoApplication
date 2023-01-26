6186692f2c030a34890e2b7420c95fdbc281c4b6
Antonio Attianese <antatt@edu.novia.fi>
2023-01-26 16:01:31 +0200

    new Todoapp

--- ToDoApplications/ToDoApp/Data/Migrations/00000000000000_CreateIdentitySchema.cs
+++ ToDoApplications/ToDoApp/Data/Migrations/00000000000000_CreateIdentitySchema.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore.Migrations;
 using System;
 
-namespace ToDoApp.Data.Migrations
+namespace TodoApp.Data.Migrations
 {
     public partial class CreateIdentitySchema : Migration
     {
--- ToDoApplications/ToDoApp/obj/Debug/net6.0/ToDoApp.AssemblyInfo.cs
+++ ToDoApplications/ToDoApp/obj/Debug/net6.0/ToDoApp.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
-[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-ToDoApp-dbf79476-98fe-4323-99d2-efce3dee2416")]
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-TodoApp-78e076a1-0079-4033-8223-02b50ba09ee8")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
